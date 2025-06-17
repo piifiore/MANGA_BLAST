f11595164b4d7ee7192abdd91c520f5737ef6a1a
ShemaleLove <rotondoluigi0@gmail.com>
2025-06-17 18:01:02 +0200

    file jdbc aggiornato

--- MANGA_BLAST/src/main/java/MyJDBC.java
+++ MANGA_BLAST/src/main/java/MyJDBC.java
@@ -5,7 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MyJDBC {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
         Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/progettotsw_db",
                 "root",
