3be71d5db9bb7db8ec67406897f3520406826c3c
piifiore <romanokevin027@gmail.com>
2025-06-18 22:57:47 +0200

    aggiunta mappatura SIGNUP servlet

--- MANGA_BLAST/src/main/java/control/SignUpServlet.java
+++ MANGA_BLAST/src/main/java/control/SignUpServlet.java
@@ -6,9 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.UserDAO;
-
 import java.io.IOException;
-//@WebServlet("/signupForm")
 public class SignUpServlet extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response)
