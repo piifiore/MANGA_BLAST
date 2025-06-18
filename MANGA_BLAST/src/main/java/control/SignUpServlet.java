8cecbebf891600a6855fd3df3e4b58a0f4e52f80
piifiore <romanokevin027@gmail.com>
2025-06-19 00:46:42 +0200

    signupservlet

--- MANGA_BLAST/src/main/java/control/SignUpServlet.java
+++ MANGA_BLAST/src/main/java/control/SignUpServlet.java
@@ -5,8 +5,10 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import model.UserDAO;
+
+import java.io.IOException;
+
 
 @WebServlet("/SignUpServlet")
 
