eb7e5d171c495630a5e11c4823b08bf58bb54f2a
ShemaleLove <rotondoluigi0@gmail.com>
2025-07-15 15:25:44 +0200

    Update ProgettoTSW.sql

--- MANGA_BLAST/src/main/db/ProgettoTSW.sql
+++ MANGA_BLAST/src/main/db/ProgettoTSW.sql
@@ -139,6 +139,7 @@ CREATE TABLE `funko` (
 
 LOCK TABLES `funko` WRITE;
 /*!40000 ALTER TABLE `funko` DISABLE KEYS */;
+INSERT INTO `funko` VALUES ('47',4.00,'images/5b4777dd-e7d2-447d-bc43-8f5aa8aa0d92_onepieice1.jpg','9898','djfy'),('69',6.90,'images/a3487c82-b45a-4a5e-9e84-acbd5ca33774_onepieice1.jpg','il 69','69696969');
 /*!40000 ALTER TABLE `funko` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -165,7 +166,7 @@ CREATE TABLE `manga` (
 
 LOCK TABLES `manga` WRITE;
 /*!40000 ALTER TABLE `manga` DISABLE KEYS */;
-INSERT INTO `manga` VALUES (9788864201795,5.20,'images/4787a49a-2376-4bbd-bd86-eae88222b174_onepieice1.jpg','One Piece vol.1','ciaocicaicn');
+INSERT INTO `manga` VALUES (456,10.00,'images/623cda69-85b9-4dc1-8ea9-624c7d864ff2_onepieice1.jpg','onepice','ujiu'),(6666666,6.66,'images/6da76804-430c-4ab6-bdad-9bd77516593c_onepieice1.jpg','666666','66666ciao'),(53625643,69.00,'images/74dcd7d7-07a6-468a-a91e-d624a3311da1_onepieice1.jpg','gay piece','il gay piece');
 /*!40000 ALTER TABLE `manga` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -179,6 +180,7 @@ DROP TABLE IF EXISTS `ordine_include_funko`;
 CREATE TABLE `ordine_include_funko` (
   `id_ordine` int NOT NULL,
   `NumeroSerie` varchar(50) NOT NULL,
+  `quantita` int NOT NULL DEFAULT '1',
   PRIMARY KEY (`id_ordine`,`NumeroSerie`),
   KEY `NumeroSerie` (`NumeroSerie`),
   CONSTRAINT `ordine_include_funko_ibfk_1` FOREIGN KEY (`id_ordine`) REFERENCES `ordini` (`id_ordine`) ON DELETE CASCADE,
@@ -205,6 +207,7 @@ DROP TABLE IF EXISTS `ordine_include_manga`;
 CREATE TABLE `ordine_include_manga` (
   `id_ordine` int NOT NULL,
   `ISBN` bigint NOT NULL,
+  `quantita` int NOT NULL DEFAULT '1',
   PRIMARY KEY (`id_ordine`,`ISBN`),
   KEY `ISBN` (`ISBN`),
   CONSTRAINT `ordine_include_manga_ibfk_1` FOREIGN KEY (`id_ordine`) REFERENCES `ordini` (`id_ordine`) ON DELETE CASCADE,
@@ -218,6 +221,7 @@ CREATE TABLE `ordine_include_manga` (
 
 LOCK TABLES `ordine_include_manga` WRITE;
 /*!40000 ALTER TABLE `ordine_include_manga` DISABLE KEYS */;
+INSERT INTO `ordine_include_manga` VALUES (1,456,2),(2,6666666,2);
 /*!40000 ALTER TABLE `ordine_include_manga` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -230,14 +234,12 @@ DROP TABLE IF EXISTS `ordini`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ordini` (
   `id_ordine` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) DEFAULT NULL,
-  `totale` decimal(10,2) DEFAULT '0.00',
-  `data` datetime DEFAULT CURRENT_TIMESTAMP,
-  `stato` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id_ordine`),
-  KEY `email` (`email`),
-  CONSTRAINT `ordini_ibfk_1` FOREIGN KEY (`email`) REFERENCES `utenti` (`email`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `email` varchar(255) NOT NULL,
+  `totale` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `data` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `stato` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_ordine`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -246,7 +248,7 @@ CREATE TABLE `ordini` (
 
 LOCK TABLES `ordini` WRITE;
 /*!40000 ALTER TABLE `ordini` DISABLE KEYS */;
-INSERT INTO `ordini` VALUES (1,'rotondoluigi0@gmail.com',25.00,'2025-07-12 16:21:03','Spedito');
+INSERT INTO `ordini` VALUES (1,'rotondoluigi0@gmail.com',20.00,'2025-07-15 15:02:53','Consegnato'),(2,'rotondoluigi0@gmail.com',13.32,'2025-07-15 15:09:08','In attesa');
 /*!40000 ALTER TABLE `ordini` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -275,7 +277,7 @@ CREATE TABLE `preferiti` (
 
 LOCK TABLES `preferiti` WRITE;
 /*!40000 ALTER TABLE `preferiti` DISABLE KEYS */;
-INSERT INTO `preferiti` VALUES ('rotondoluigi0@gmail.com','manga','9788864201795');
+INSERT INTO `preferiti` VALUES ('rotondoluigi0@gmail.com','manga','456'),('rotondoluigi0@gmail.com','manga','9788864201795');
 /*!40000 ALTER TABLE `preferiti` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -300,7 +302,7 @@ CREATE TABLE `utenti` (
 
 LOCK TABLES `utenti` WRITE;
 /*!40000 ALTER TABLE `utenti` DISABLE KEYS */;
-INSERT INTO `utenti` VALUES ('rotondoluigi0@gmail.com','Password1@','ciao sono bello\r\n');
+INSERT INTO `utenti` VALUES ('rotondoluigi0@gmail.com','Password1@','ciao sono bello, ciao\r\n');
 /*!40000 ALTER TABLE `utenti` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -313,4 +315,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-07-14 19:49:49
+-- Dump completed on 2025-07-15 15:25:20
